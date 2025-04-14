093faf3d58a4728a5be8b641363898ce8d2e2c99
Rubens Santos <rubenssegundo404@gmail.com>
2025-04-14 19:01:55 -0300

    p1: transforming data

--- islands_engine/lib/islands_engine/guesses.ex
+++ islands_engine/lib/islands_engine/guesses.ex
@@ -1,5 +1,5 @@
 defmodule IslandsEngine.Guesses do
-  alias __MODULE__
+  alias IslandsEngine.{Coordinate, Guesses}
 
   @enforce_keys [:hits, :misses]
   defstruct [:hits, :misses]
@@ -9,4 +9,10 @@ defmodule IslandsEngine.Guesses do
       hits: MapSet.new(),
       misses: MapSet.new()
     }
+
+  def add(%Guesses{} = guesses,:hit, %Coordinate{} = coordinate), do:
+    update_in(guesses.hits, &MapSet.put(&1, coordinate))
+
+  def add(%Guesses{} = guesses, :miss, %Coordinate{} = coordinate), do:
+    update_in(guesses.misses, &MapSet.put(&1, coordinate))
 end
